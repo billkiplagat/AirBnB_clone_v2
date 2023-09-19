58603e6d8f96520c2e19746dfe227e038f00353e
vierukundo <vierukundo@gmail.com>
2023-09-19 08:58:15 -0700

    Changed the password of User hbnb_dev to hbnb_dev_pwd

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- Create the hbnb_dev_db database if it doesn't exist
 -- Create the hbnb_dev user if it doesn't exist and set the password
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'root';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
