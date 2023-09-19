95c59268dfb6ae64b53d14d34b640ec698bac9b9
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-19 10:34:11 +0300

    Updated sql test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,7 @@
 -- Create the hbnb_dev_db database if it doesn't exist
 -- Create the hbnb_dev user if it doesn't exist and set the password
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'root';
-
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
