d1be50316eb606abbc1db5005a4e9d722c1ff3de
billkiplagat <bill.kiplagat.7@gmail.com>
2023-10-20 10:46:17 +0300

    Updated more routes

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+listening on 0.0.0.0, port 5000
+with Fifth routes
+"""
+from flask import Flask
+import re
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ First Route that display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb2():
+    """ Second Route that display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    """ Third Route that display C and text"""
+    text = re.sub(r'_', ' ', text)
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_with_text(text):
+    """ Fourth Route that display Python and text"""
+    text = re.sub(r'_', ' ', text)
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_is_integer(n):
+    """ Fifth Route that display C and text """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
