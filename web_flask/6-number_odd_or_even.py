b41ad55c6da3c3eea94c7745b1b878745c6df365
billkiplagat <bill.kiplagat.7@gmail.com>
2023-10-21 13:44:36 +0300

    Updated state template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -54,8 +54,7 @@ def number_template(n):
 def number_odd_or_even(n):
     """Returns a template at the /number_odd_or_even/<n>
         route, display if the number is odd or even"""
-    odd_or_even = 'even' if n % 2 == 0 else 'odd'
-    return render_template('6-number_odd_or_even.html', number=n, odd_or_even=odd_or_even)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == '__main__':
